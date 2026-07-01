3d34f4b4b09978014e107f811eb07c2b158492ef
Karolay <luedk.garcia@autonoma.edu.co>
2026-07-01 13:50:35 -0500

    Se añaden los metodos de la pila

--- src/main/java/Stack.java
+++ src/main/java/Stack.java
@@ -2,5 +2,32 @@ public class Stack {
     Nodo top;
     int tamanio;
 
+    public void push(int valor) {
+        Nodo nuevo = new Nodo(valor);
+        nuevo.siguiente = top;
+        top = nuevo;
+        tamanio++;
+    }
 
+    public int pop() {
+        int valor = top.valor;
+        top.siguiente = top;
+        tamanio--;
+        return valor;
+    }
+
+    public int peek() {
+        return top.valor;
+    }
+
+    public int size(){
+        return tamanio;
+    }
+
+    public boolean isEmpty(){
+        if (tamanio == 0){
+            return true;
+        }
+        return false;
+    }
 }
